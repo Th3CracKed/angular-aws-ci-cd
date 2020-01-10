09a32ef2debb71e2eb77b1f87d1857be4a4b3061
abdel <abdelghafour.driowya@skopai.com>
2020-01-10 16:40:36 +0100

    reverse karma config, and support code coverage

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -6,6 +6,7 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
+      require('karma-junit-reporter'),
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
@@ -20,17 +21,30 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
+
     customLaunchers: {
       ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
+        base: 'Chrome',
         flags: [
           '--no-sandbox',
           '--disable-gpu',
-          '--no-sandbox'
+          '--headless',
+          '--remote-debugging-port=9222'
         ]
       }
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'junit'],
+    // the default configuration
+    junitReporter: {
+      outputDir: '', // results will be saved as $outputDir/$browserName.xml
+      outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
+      suite: '', // suite will become the package name attribute in xml testsuite element
+      useBrowserName: true, // add browser name to report and classes names
+      nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+      classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+      properties: {}, // key value pair of properties to add to the <properties> section of the report
+      xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
