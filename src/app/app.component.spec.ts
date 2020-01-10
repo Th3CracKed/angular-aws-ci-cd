3e7d1c3640f4abe8f99cb46d88951d41c8f8d61d
abdel <abdelghafour.driowya@skopai.com>
2020-01-10 18:06:55 +0100

    failing test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'my-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-app');
+    expect(app.title).toEqual('my-app2');
   });
 
   it('should render title in a h1 tag', () => {
